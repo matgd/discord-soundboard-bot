e832d012e956fde211e6b502ff6f857b19b1f406
matgd <28411337+matgd@users.noreply.github.com>
2025-01-16 16:15:13 +0100

    Random play

---
+++ commands/utility/play-sound-random.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { playSoundAndReply } = require('../../utils');
+
+
+module.exports = {
+    cooldown: 5,
+	data: new SlashCommandBuilder()
+		.setName('randomplay')
+		.setDescription('Play random sound.')
+    	.setDescriptionLocalizations({
+            pl: 'Odtwórz losowo jeden z dostępnych dźwięków.',
+        }),
+    async execute(interaction) {
+        const soundsIds = interaction.client.soundsIds;
+        const randomSoundId = soundsIds[Math.floor(Math.random() * soundsIds.length)];
+        await playSoundAndReply(interaction, randomSoundId, `:game_die: **Playing:** ${randomSoundId}`);
+	}
+};
+
+
