ed8265f6a628ba4df3225a7d0e7deb014ddd10d0
matgd <28411337+matgd@users.noreply.github.com>
2025-09-21 13:33:42 +0200

    refactor: remove unused fs and path imports from index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const fs = require("node:fs");
-const path = require("node:path");
-const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require("discord.js");
+const { Client, Events, GatewayIntentBits, MessageFlags } = require("discord.js");
 const { token } = require("./config.json");
-const { basenameToId, disconnectBotFromVoiceChannel, humanCountInVoiceChannel, ensureDataStoreExists, loadCommands, loadSoundIds } = require("./utils");
+const { disconnectBotFromVoiceChannel, humanCountInVoiceChannel, ensureDataStoreExists, loadCommands, loadSoundIds } = require("./utils");
 const { ActivityType } = require("discord.js");
 
 ensureDataStoreExists();
