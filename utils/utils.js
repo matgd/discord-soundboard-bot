98fd395ca385a0b426ae3d575032b48a298ed034
matgd <28411337+matgd@users.noreply.github.com>
2026-01-11 20:55:06 +0100

    fix: update basenameToId function to allow brackets and parentheses in normalized IDs

--- utils/utils.js
+++ utils/utils.js
@@ -33,7 +33,7 @@ function replacePolishChars(str) {
  */
 function basenameToId(basename) {
     let modifiedBasename = replacePolishChars(basename);
-    modifiedBasename = modifiedBasename.replace(/[^a-zA-Z0-9]/g, " ");
+    modifiedBasename = modifiedBasename.replace(/[^a-zA-Z0-9\[\]\(\)]/g, " ");
     modifiedBasename = modifiedBasename.replace("  ", ") ");
     return modifiedBasename.toLowerCase();
 }
