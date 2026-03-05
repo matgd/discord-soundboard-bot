d225361881d653459ed5b6da6652b84b3d2916fb
matgd <28411337+matgd@users.noreply.github.com>
2026-03-05 19:19:43 +0100

    sync_bot_sounds.sh improvements

--- sync_bot_sounds.sh
+++ sync_bot_sounds.sh
@@ -17,8 +17,10 @@ if [ $? -ne 0 ]; then
     exit 2
 fi
 
-echo "[1/2] Removing peaks folder"
+echo "> Removing $LOCAL_DIR/peaks folder..." 
 rm -rv "$LOCAL_DIR/peaks"
+echo "< Done."
 
-echo "[2/2] Performing SSH rsync"
+echo "> Performing SSH rsync..."
 rsync -chavzP --stats --delete "$LOCAL_DIR" "$REMOTE_HOST:$REMOTE_DIR"
+echo "< Done."
